7f0e264e7d4121f7f21c0b248fa2f0578dbbbdc0
nazmul162001 <nazmulprime162001@gmail.com>
2022-07-25 13:53:26 +0600

    added increment and decrement function

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,16 +1,31 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux/es/exports';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrementCounter, incrementCounter, resetCounter } from '../services/action/counterAction';
+
 
 const Counter = () => {
 
   const count = useSelector((state) => state.count)
+  const dispatch = useDispatch()
+
+  const handleIncrement = ()=> {
+    dispatch(incrementCounter())
+  }
+  const handleDecrement = ()=> {
+    dispatch(decrementCounter())
+  }
+  const handleReset = ()=> {
+    dispatch(resetCounter())
+  }
 
   return (
     <div>
       <h2>Counter App</h2>
 
       <h3>Count : {count} </h3>
-      <button>Increment</button>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decriment</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 };
