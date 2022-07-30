d39c621b62df720e183251564e1b4924fa8a7901
nazmul162001 <nazmulprime162001@gmail.com>
2022-07-30 19:31:45 +0600

    setUp redux toolkit & folder structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
       <h2>Welcome to Redux counter app</h2>
-      <Counter />
     </div>
   );
 }
---
+++ src/features/counter/counterSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: { count: 0 },
+  reducers: {
+    increment: (state) => {
+      state.count = state.count + 1;
+    },
+    decrement: (state) => {
+      state.count = state.count - 1;
+    },
+    reset: (state) => {
+      state.count = 0;
+    },
+  },
+});
