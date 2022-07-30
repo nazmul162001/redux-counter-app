ec1f8150004af604933a5f8320830117210be681
nazmul162001 <nazmulprime162001@gmail.com>
2022-07-30 20:27:30 +0600

    added custom increment & decrement

--- src/features/counter/CounterView.js
+++ src/features/counter/CounterView.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux/es/exports';
-import { decrement, increment, reset } from './counterSlice';
+import { counterByAmount, counterByAmountMinus, decrement, increment, reset } from './counterSlice';
 
 const CounterView = () => {
   const count = useSelector((state) => state.counter.count);
+  const [value, setValue] = useState(0)
+
+  // console.log(value);
 
   const dispatch = useDispatch();
 
+  const handleChange = e => {
+    // console.log(e.target.value);
+    // setValue(e.target.value)
+    const getValue = parseInt(e.target.value);
+    setValue(getValue);
+  }
+
+
   return (
     <div>
+      <input onChange={handleChange} type="text" name="" id="" />
       <h2>count: {count} </h2>
       <button
         style={{margin: "0 10px"}}
@@ -34,6 +46,22 @@ const CounterView = () => {
         }}
       >
         Reset
+      </button>
+      <button
+        style={{margin: "0 10px"}}
+        onClick={() => {
+          dispatch(counterByAmount(value));
+        }}
+      >
+        Custom Increment
+      </button>
+      <button
+        style={{margin: "0 10px"}}
+        onClick={() => {
+          dispatch(counterByAmountMinus(value));
+        }}
+      >
+        Custom Decrement
       </button>
     </div>
   );
--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -13,9 +13,15 @@ export const counterSlice = createSlice({
     reset: (state) => {
       state.count = 0;
     },
+    counterByAmount: (state, action) => {
+      state.count = state.count + action.payload;
+    },
+    counterByAmountMinus: (state, action) => {
+      state.count = state.count - action.payload;
+    },
   },
 });
 
-export const { increment, decrement, reset } = counterSlice.actions;
+export const { increment, decrement, reset, counterByAmount, counterByAmountMinus } = counterSlice.actions;
 
 export default counterSlice.reducer;
